89f54c62c509c154e2f2efe430d1b8c496ef569c
Alex <mathewalex806@gmail.com>
2025-07-31 10:42:29 +0530

    first commit

---
+++ billsplit/testapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BaseMode(models.Model):
+    name = models.CharField(max_length=24)
+    amount = models.IntegerField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}----> {self.amount}----> {self.created_at}"
